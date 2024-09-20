f0346f4702284d569f1273ca2a29ad3f40981f17
vaibhavRaj7667 <zivaibhav1@gmail.com>
2024-09-20 11:29:30 +0530

    check

--- test2.js
+++ test2.js
@@ -37,4 +37,30 @@
 // username = username.trim();
 // console.log(username);
 
-//------------------------------------------------------
+//------------------------------------------------------ maths
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.sqrt(49));
+
+// console.log(Math.random());
+// console.log(Math.random() * 10 + 1);
+
+// const min = 10;
+// const max = 20;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+//++++++++++++++++++++Arrays
+
+const myarra = [5, 4, 3, 2, 1, 0];
+const myheros = ["a", "b", "c", "d"];
+
+// myarra.push(11);
+// myarra.pop();
+
+console.log(myarra.includes(33));
+
+console.log(myarra);
