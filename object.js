7c2707d98652643b0bbbf941196e77e1f9407061
vaibhavRaj7667 <zivaibhav1@gmail.com>
2024-09-23 18:12:20 +0530

    pratice

---
+++ object.js
@@ -0,0 +1,20 @@
+const my = {
+  Name: " vaibhav raj",
+  class: "BCA",
+  admission_no: "22SCSE1110021",
+};
+// yo dont have to use dote operator again just use name to access
+const { admission_no: bro } = my;
+console.log(bro);
+
+let total=0;
+function add(...give){
+  for(let i=0;i<arguments.length;i++){
+    total += give[i];
+  }
+
+}
+add(5,5,5,5,5,5)
+
+console.log(total)
+
