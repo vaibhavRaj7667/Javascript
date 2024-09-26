f23f456ad3a1e28e5eccf14c52c4549283b9688a
vaibhavRaj7667 <zivaibhav1@gmail.com>
2024-09-27 00:34:25 +0530

    tip calculator

---
+++ tip calculator/dot.js
@@ -0,0 +1,17 @@
+
+const amount = document.getElementById('amount')
+const tip = document.getElementById('tip')
+const btn = document.getElementById('btn')
+const result = document.getElementById('result')
+
+btn.addEventListener('click', function() {
+    const amountValue = parseFloat(amount.value);
+    const tipValue = parseFloat(tip.value);
+
+    if (!isNaN(amountValue) && !isNaN(tipValue)) {  
+        const answer = (amountValue * tipValue) / 100;
+        result.textContent = `$${answer.toFixed(2)}`;  
+    } else {
+        result.textContent = 'Please enter valid numbers!';  
+    }
+});
